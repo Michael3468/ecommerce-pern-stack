94cd780b48f7ae47ae3fe3c3d4b0769a5a9c6adf
Mikhail <mikhail3468@yandex.ru>
2023-03-10 15:06:26 +0300

    feat: add controllers with fileUpload and uuid

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,6 +1,27 @@
+const ApiError = require('../error/ApiError');
+const { Brand } = require('../models/models');
+
 class BrandController {
-  async create(req, res) {}
-  async getAll(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name } = req.body;
+      const brand = await Brand.create({ name });
+      return res.json(brand);
+    } catch (err) {
+      console.error(err);
+      next(ApiError.badRequest(err.message));
+    }
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const brands = await Brand.findAll();
+      return res.json(brands);
+    } catch (err) {
+      console.error(err);
+      next(ApiError.badRequest(err.message));
+    }
+  }
 }
 
 module.exports = new BrandController();
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,6 +1,26 @@
+const { Type } = require('../models/models');
+const ApiError = require('../error/ApiError');
+
 class TypeController {
-  async create(req, res) {}
-  async getAll(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name } = req.body;
+      const type = await Type.create({ name });
+      return res.json(type);
+    } catch (err) {
+      console.error(err);
+      next(ApiError.badRequest(err.message));
+    }
+  }
+  async getAll(req, res, next) {
+    try {
+      const types = await Type.findAll();
+      return res.json(types);
+    } catch (err) {
+      console.error(err);
+      next(ApiError.badRequest(err.message));
+    }
+  }
 }
 
 module.exports = new TypeController();
