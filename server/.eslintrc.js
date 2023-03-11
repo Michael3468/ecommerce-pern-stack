b26cc1673e649e68919c2f819b86e9c36ec88f17
Mikhail <mikhail3468@yandex.ru>
2023-03-11 17:32:50 +0300

    build: add eslint-plugin-import to lint imports order

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,9 +1,27 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  parser: '@typescript-eslint/parser',
 
   parserOptions: {
-    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    // project: './tsconfig.json', // ??
   },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+
+    // eslint-plugin-import (fix imports order)
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+
+  // eslint-plugin-import (fix imports order)
+  plugins: ['import', '@typescript-eslint'],
 
   rules: {
     'node/exports-style': ['error', 'module.exports'],
@@ -15,5 +33,42 @@ module.exports = {
     'node/prefer-global/url': ['error', 'always'],
     'node/prefer-promises/dns': 'error',
     'node/prefer-promises/fs': 'error',
+
+    'no-console': 2,
+
+    // eslint-plugin-import (fix imports order)
+    'import/no-extraneous-dependencies': 1,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+
+  settings: {
+    // eslint-plugin-import (fix imports order)
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
