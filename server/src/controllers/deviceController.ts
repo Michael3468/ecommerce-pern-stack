e8db03130b42dfbe31f84617d7e052d1220ca7c8
Mikhail <mikhail3468@yandex.ru>
2023-03-16 15:25:51 +0300

    refactor(devicecontroller.ts): change TRequest.files.img type from 'any' to 'UploadedFile'

--- server/src/controllers/deviceController.ts
+++ server/src/controllers/deviceController.ts
@@ -1,8 +1,9 @@
 /* eslint-disable class-methods-use-this */
 import { Request, Response, NextFunction } from 'express';
+import { UploadedFile } from 'express-fileupload';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { v4 as createUuid } from 'uuid';
+// import { v4 as createUuid } from 'uuid'; // TODO del uuid from package.json
 
 import ApiError from '../error/ApiError';
 import { Device } from '../models/models';
@@ -10,9 +11,9 @@ import { Device } from '../models/models';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-type IReq = {
+type TRequest = {
   files: {
-    img: any; // TODO: any
+    img: UploadedFile;
   };
 };
 
@@ -20,17 +21,23 @@ class DeviceController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
       const { name, price, brandId, typeId, info } = req.body;
-      const { img } = (req as unknown as IReq).files;
-      const fileName = `${createUuid()}.jpg`;
-      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+      const { img } = (req as unknown as TRequest).files || null;
 
-      const device = await Device.create({ name, price, brandId, typeId, img: fileName });
-      return res.json(device);
+      if (img) {
+        // TODO: move to libs getMd5FileName(imgFile: UploadedFile) +
+        const fileExtension = img.name.split('.').pop();
+        const fileName = `${img.md5}.${fileExtension}`;
+        // TODO: move to libs getMd5FileName(imgFile: UploadedFile) -
+        img.mv(path.resolve(__dirname, '../../static', fileName));
+
+        const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+        return res.json(device);
+      }
     } catch (err) {
       next(ApiError.badRequest((err as ApiError).message));
     }
 
-    return res.status(404).json({ error: 'Resource not found' });
+    return res.status(404).json({ error: 'Could not create device' });
   }
 
   async getAll(req: Request, res: Response) {}
