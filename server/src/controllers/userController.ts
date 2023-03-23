05bc2431095bfd99fd04c728b0d9ffecc6cee4c0
Mikhail <mikhail3468@yandex.ru>
2023-03-23 14:00:35 +0300

    feat(usercontroller): add 'login' method

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -29,7 +29,25 @@ class UserController {
     return res.json({ token });
   }
 
-  async login(req: Request, res: Response): Promise<void> {}
+  async login(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return next(ApiError.internal('User not found'));
+      }
+
+      const isValidPassword = bcrypt.compareSync(password, user.password);
+      if (!isValidPassword) {
+        return next(ApiError.internal('Password is not correct'));
+      }
+
+      const token = generateJwt(user.id, user.email, user.role);
+      return res.json({ token });
+    } catch (err) {
+      return next(ApiError.internal((err as Error).message));
+    }
+  }
 
   async check(req: Request, res: Response, next: NextFunction): Promise<Response> {
     const id = req.query?.id ?? '';
