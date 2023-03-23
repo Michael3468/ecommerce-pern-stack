c0d694f4683f517605d27bbda789af63f36df9fd
Mikhail <mikhail3468@yandex.ru>
2023-03-23 15:32:58 +0300

    refactor(errorhandlingmiddleware): add 'next' parameter to errorHandler function

--- server/src/middleware/ErrorHandlingMiddleware.ts
+++ server/src/middleware/ErrorHandlingMiddleware.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import ApiError from '../error/ApiError';
 
-function errorHandler(err: Error, req: Request, res: Response) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
   }
