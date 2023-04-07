3474b96a18548fa70aa3fc4eb5f0e018e444fe8d
Mikhail <mikhail3468@yandex.ru>
2023-04-07 14:32:59 +0300

    feat(axios): add axios config

---
+++ client/src/http/index.ts
@@ -0,0 +1,23 @@
+import axios, { InternalAxiosRequestConfig, AxiosRequestConfig } from 'axios';
+
+const $host = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const $authHost = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const authInterceptor = (
+  config: InternalAxiosRequestConfig<AxiosRequestConfig>,
+): InternalAxiosRequestConfig<AxiosRequestConfig> => {
+  const configWithInterceptor = { ...config };
+
+  configWithInterceptor.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
+
+  return configWithInterceptor;
+};
+
+$authHost.interceptors.request.use(authInterceptor);
+
+export { $host, $authHost };
