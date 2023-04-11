f2035b641dcc288e8ad082e6b607823c19c333f4
Mikhail <mikhail3468@yandex.ru>
2023-04-11 09:23:39 +0300

    refactor(devicecontroller.ts): add copy 'no-image.png' to 'static' if it does not exist on create

--- server/src/controllers/deviceController.ts
+++ server/src/controllers/deviceController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import fs from 'fs';
 import path from 'path';
 
 import ApiError from '../error/ApiError';
@@ -34,9 +35,26 @@ class DeviceController {
 
         device = await Device.create({ name, price, brandId, typeId, img: fileName });
       } else {
-        // TODO: 'add default-image.jpg'
-        const defaultImage = path.resolve(__dirname, '../static', 'default-image.jpg');
-        device = await Device.create({ name, price, brandId, typeId, img: defaultImage });
+        /**
+         * check if 'no-image.png' exist in 'static/ dir
+         * if not copy it
+         */
+        const destinationPath = path.resolve(__dirname, '../static');
+        const imageName = 'no-image.png';
+
+        fs.access(`${destinationPath}/${imageName}`, fs.constants.F_OK, (error) => {
+          if (error) {
+            console.log(error);
+
+            const sourcePath = path.resolve(__dirname, '../assets/images', imageName);
+            fs.copyFile(sourcePath, `${destinationPath}/${imageName}`, (err) => {
+              if (err) throw Error;
+              console.log('File was copied successfully');
+            });
+          }
+        });
+
+        device = await Device.create({ name, price, brandId, typeId, img: imageName });
       }
 
       if (info) {
