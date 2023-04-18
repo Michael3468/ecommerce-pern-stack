91a04f88f33eee5b24920865401532ad39ea89d5
Mikhail <mikhail3468@yandex.ru>
2023-04-18 17:08:08 +0300

    refactor(loginbutton.tsx): move login button to separate component

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,17 +1,17 @@
 import { observer } from 'mobx-react-lite';
 import { useContext } from 'react';
-import { Container, Navbar, Nav, Button } from 'react-bootstrap';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 import { USER_ROLE, ROUTE, mainTheme } from '../constants';
 import { StoreContext } from '../index';
 import AdminPanelButton from './AdminPanelButton';
 import CartButton from './CartButton';
+import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 
 const Navigation = observer(() => {
   const { userStore } = useContext(StoreContext);
-  const navigate = useNavigate();
 
   return (
     <Navbar bg="dark" variant="dark">
@@ -23,18 +23,13 @@ const Navigation = observer(() => {
         {userStore.isAuth ? (
           <Nav style={{ color: mainTheme.link.color }}>
             <CartButton />
-
             {userStore.user?.role === USER_ROLE.ADMIN && <AdminPanelButton />}
-
             <LogoutButton />
           </Nav>
         ) : (
           <Nav style={{ color: mainTheme.link.color }}>
             <CartButton />
-
-            <Button variant="outline-light" onClick={() => navigate(ROUTE.LOGIN)}>
-              Log In
-            </Button>
+            <LoginButton />
           </Nav>
         )}
       </Container>
